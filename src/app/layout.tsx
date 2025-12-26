b09fda13c7f4f6a1bbfa11e2801afdef6cc3c9ad
qkitzero <qkitzero@gmail.com>
2025-12-26 19:53:37 +0900

    add: header (#5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -23,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased p-8`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
